aa4098816c41d0d3dabad9402069a1f4a8cd13f4
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-04-13 21:18:43 +0600

    navbar added and react icons used in navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme = "light"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <main className="container mx-auto space-y-20">
+           {children}
+        </main>
+      </body>
     </html>
   );
 }
